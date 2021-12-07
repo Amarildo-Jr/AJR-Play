6a7a02f4dd03e2607c4c5658158031c3af03d978
Amarildo-Jr <ajunior283@outlook.com>
2021-12-07 13:31:56 -0300

    Colocando filmes em formato de carrossel

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 var idFilmes = 0;
 
 class Filme {
-  constructor(nome, imagem, trailer, lancamento, banner) {
+  constructor(nome, imagem, trailer, lancamento) {
     this.id = idFilmes;
     this.nome = nome;
     this.lancamento = lancamento;
     this.imagem = imagem;
     this.trailer = trailer;
+    //this.banner = banner;
     //this.genero = genero;
     idFilmes++;
   } 
 }
 
-let filme0 = new Filme("O Rei do Show", "img/posters/reiDoShow.jpg", "https://www.youtube.com/embed/r5R6CVp_JzU", "2017");
+let filme0 = new Filme("O Rei do Show", "img/posters/reiDoShow.jpg", "https://www.youtube.com/embed/r5R6CVp_JzU", "2017", "https://miro.medium.com/max/1400/0*SWX3l0qWLUruMP3p.jpg");
 let filme1 = new Filme("Homem-aranha: Sem Volta pra Casa", "img/posters/homemAranha.jpg", "https://youtube.com/embed/rzOvXvBNzMc", "2021");
 let filme2 = new Filme("O Jogo da Imitação", "img/posters/jogoDaImitacao.jpeg", "https://youtube.com/embed/YIkKbMcJL_4", "2014");
 let filme3 = new Filme("Your name", "img/posters/yourName.jpg", "https://youtube.com/embed/soQXM3XVvIU", "2016");
@@ -61,16 +62,33 @@ function abrirDetalhes(id) {
   var popUp = document.getElementsByClassName("popUp");
   popUp[0].style.display = "block";
   var popUp_content = document.getElementsByClassName("details");
-  popUp_content[0].innerHTML = "<h2>" + filme.nome + " 🎬 </h2>" +
-  "<iframe class='video' width='700' height='400' src=\"" + filme.trailer + "\" title='" + filme.nome + 
+  popUp_content[0].innerHTML = "<iframe class='video' width='700' height='400' src=\"" + filme.trailer + "\" title='" + filme.nome + 
   "' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>" +
-  "<div id='details'><h4>" + filme.lancamento + "</h4><img src='" + filme.imagem + "'>" +
+  "<h2>" + filme.nome + " 🎬 </h2>" +
+  "<div id='details'><h4>" + filme.lancamento + "</h4>" +
   "<p class='descricao_filme'>Isso é uma descrição do filme e tudo mais, aqui deve ter todas as informações como: sinopse, principais atores e avaliação</p></div>" +
-  "<style>.video {margin-bottom: 25px; display: inline;} #details img {float: left; margin-right: 20px;} #details p {text-align: left;} </style>";
+  "<style> .video {margin-bottom: 25px;z-index: 2;} " +
+  ".details {background: url('" + filme.banner + "'); background-repeat: no-repeat;background-position: top;background-size: 100% auto;} #details {margin-top: 50px}" +
+  "#details img {float: left;} #details {margin-top: 300px;}" +
+  "#details p {text-align: left;} </style>";
 }
 
-for(i = 0; i < filmes.length; i++) {
-  document.write("<div class='div_poster'><input type='image' src=" + filmes[i].imagem + 
-  " class='poster' onclick=\"abrirDetalhes('" + filmes[i].id + "')\"></div>")
+document.write("<div class='filmes'><section id='section0'><a href='#section2' class='arrow_btn'>‹</a>")
+j = 0;
+for(i = 1; i <= filmes.length; i++) {
+  document.write("<div class='div_poster'><input type='image' src=" + filmes[i-1].imagem + 
+  " class='poster' onclick=\"abrirDetalhes('" + filmes[i-1].id + "')\"></div>")
+
+  if (j == 2 && i == filmes.length){
+    document.write("<a href='#section0' class='arrow_btn'>›</a></section>");
+    j++;
+  }
+
+  else if(i % 5 == 0 && i > 1){
+    document.write("<a href='#section" + (j + 1) + "' class='arrow_btn'>›</a></section><section id='section" + (j + 1) + "'><a href='#section" + j + "' class='arrow_btn'>‹</a>");
+    j++;
+  }
+  
 }
 
+document.write("</div>")
