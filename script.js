41a4a30f128d388dbd97379ab07bb1102335bd9e
Amarildo-Jr <ajunior283@outlook.com>
2022-01-20 16:38:50 -0300

    Consertando erro do poster do Rambo

--- script.js
+++ script.js
@@ -608,7 +608,7 @@ let filme40 = new Filme(
 
 let filme41 = new Filme(
   "Rambo: Até o fim", 
-  "img/posters/filmes/ramboAteOFIM.jpg", 
+  "img/posters/filmes/ramboAteOFim.jpg", 
   "https://www.youtube.com/embed/yd71hmhobAg", 
   "2019",
   ["Ação", "Aventura"],
